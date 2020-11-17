64ba86d2a0a7d86321c4904b8da420dfb69accd8
Dan Ste. Marie Jr <DSte.mariejr@gmail.com>
2020-11-17 17:59:02 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(results) {
+  const win = results.find( ({result}) => result === 'W');
+  if (!!win) {
+    return win.year
+  } else {
+    win
+  }
+}
